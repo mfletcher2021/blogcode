156d7d5d955b72a2279eef60c036d34c1308ba06
mfletcher2021 <80551291+mfletcher2021@users.noreply.github.com>
2024-12-04 21:27:58 -0600

    Add files via upload

---
+++ TypesVariablesandComments.cs
@@ -0,0 +1,6 @@
+﻿// This code is writing a simple sentence with both string and int variables
+int year = 2024;
+string name = "Michael";
+string month = "December";
+
+Console.WriteLine(name + " is writing this post in " + month + " " + year);

892adf60d33199458842d7a374c557efc3105e34
mfletcher2021 <80551291+mfletcher2021@users.noreply.github.com>
2024-12-16 20:52:49 -0600

    Add files via upload

---
+++ MathClass.cs
@@ -0,0 +1,17 @@
+﻿// A demo of some basic C# Math class methods
+Console.WriteLine(Math.Max(3, 12));
+Console.WriteLine(Math.Abs(-333.22));
+Console.WriteLine(Math.Min(3, 12));
+Console.WriteLine(Math.Sqrt(49));
+
+//A demo of some more complex C# Math class methods
+Console.WriteLine(Math.Pow(2, 5));
+Console.WriteLine(Math.Log10(14));
+Console.WriteLine(Math.Cos(84));
+Console.WriteLine(Math.Round(45.2423242152332, 2));
+
+//A demo of the same complex C# Math class methods using e-notation
+Console.WriteLine(Math.Pow(4e-5, 5));
+Console.WriteLine(Math.Log10(16e4));
+Console.WriteLine(Math.Cos(8e1));
+Console.WriteLine(Math.Round(42e5+2.16, 2));

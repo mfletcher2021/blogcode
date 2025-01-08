99d71f2f431f210898cfaac5c80f849e50807e64
mfletcher2021 <80551291+mfletcher2021@users.noreply.github.com>
2025-01-07 21:56:53 -0600

    Add files via upload
    First commit of 2025

---
+++ UserInputTest.cs
@@ -0,0 +1,6 @@
+﻿// Asking user input for today's date
+Console.WriteLine("Please enter today's date");
+
+string todaysDate = Console.ReadLine();
+
+Console.WriteLine("Today's date is: " + todaysDate);

62688118660360417c04c3a5e531000a075dc18d
mfletcher2021 <80551291+mfletcher2021@users.noreply.github.com>
2025-02-16 21:39:34 -0600

    Add files via upload

---
+++ 1darray.cs
@@ -0,0 +1,31 @@
+﻿//Simple C# array example
+string[] languages = ["Python", "R", "Java", "MySQL", "CSS", "Bootstrap", "HTML", "C#", "GitHub"];
+
+Console.WriteLine(languages[2]);
+
+//sorting array alphabetically
+Array.Sort(languages);
+Console.WriteLine();
+
+foreach (string l in languages)
+    {
+    Console.WriteLine(l);
+}
+
+//reverse sorting array
+Console.WriteLine();
+Array.Reverse(languages);
+
+foreach (string l in languages)
+{
+    Console.WriteLine(l);
+}
+
+//modifying an array
+Console.WriteLine();
+languages[1] = "JavaScript";
+Console.WriteLine(string.Join(", ", languages));
+
+//how long is the array
+Console.WriteLine();
+Console.WriteLine(languages.Length);

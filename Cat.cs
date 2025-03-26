3d8ea64fcd1e97585bf97dd2f64db61eb0886f6e
mfletcher2021 <80551291+mfletcher2021@users.noreply.github.com>
2025-03-25 21:32:20 -0500

    Add files via upload

---
+++ Cat.cs
@@ -0,0 +1,14 @@
+﻿//creating a Cat class
+class Cat
+{
+    string color = "orange";
+    int age = 11;
+    string gender = "male";
+
+    static void Main(string[] args)
+    {
+        Cat orangeCat = new Cat();
+        Console.WriteLine(orangeCat.age);
+
+    }
+}
---
+++ NewCat.cs
@@ -0,0 +1,20 @@
+﻿class NewCat
+{
+    string color;
+    int age;
+    string gender;
+
+    public NewCat(string catColor, int catAge, string catGender)
+    {
+        color = catColor;
+        age = catAge;
+        gender = catGender;
+    }
+
+    static void Main(string[] args)
+    {
+        NewCat brownCat = new NewCat("brown", 11, "female");
+        Console.WriteLine("This is a " + brownCat.color + " cat who is " + brownCat.age + " years old and is a " + brownCat.gender);
+
+    }
+}
